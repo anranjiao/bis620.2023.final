a951ef475decc8042b82e0dbac3464ad630ba854
MinZhu123 <84722601+MinZhu123@users.noreply.github.com>
2023-12-20 11:33:15 -0500

    Update neural_network

--- tests/testthat/test-neural_network.R
+++ tests/testthat/test-neural_network.R
@@ -2,11 +2,5 @@ test_that("neural_network() works", {
   data("adsl")
   data("biomark")
   dataset = preprocessing(adsl, biomark, 600)
-  x_train=torch_tensor(as.matrix(dataset$x_train))
-  y_train=torch_tensor(as.matrix(dataset$y_train))
-  x_test=torch_tensor(as.matrix(dataset$x_test))
-  y_test=torch_tensor(as.matrix(dataset$y_test))
-
-  expect_failure(expect_equal(neural_network(x_train,y_train,x_test,y_test,dataset$min_label,dataset$max_label, 5),
-                              neural_network(x_train,y_train,x_test,y_test,dataset$min_label,dataset$max_label, 5)))
+  expect_failure(expect_equal(neural_network(dataset, 5), neural_network(dataset, 5)))
 })
